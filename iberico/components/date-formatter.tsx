cfe79875d5b8f4df2995cfb6ec8e2aae93d777da
tkawamura <tkawamura.stayhome@gmail.com>
2022-06-25 12:24:56 +0900

    日付のフォーマットを日本の表記に変更

--- iberico/components/date-formatter.tsx
+++ iberico/components/date-formatter.tsx
@@ -6,7 +6,7 @@ type Props = {
 
 const DateFormatter = ({ dateString }: Props) => {
   const date = parseISO(dateString)
-  return <time dateTime={dateString}>{format(date, 'LLLL	d, yyyy')}</time>
+  return <time dateTime={dateString}>{format(date, 'yyyy年M月d日')}</time>
 }
 
 export default DateFormatter
