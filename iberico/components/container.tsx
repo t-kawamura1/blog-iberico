489a3ce1ba84bd035d585dec1c4e0c0bdc7f6810
tkawamura <tkawamura.stayhome@gmail.com>
2022-06-25 13:17:32 +0900

    不要なpropsを削除、コメント追記

--- iberico/components/container.tsx
+++ iberico/components/container.tsx
@@ -4,6 +4,7 @@ type Props = {
   children?: ReactNode
 }
 
+/** 要素中央よせ・レスポンシブに対応するコンポーネント */
 const Container: FunctionComponent = ({ children }: Props) => {
   return <div className="container mx-auto px-5">{children}</div>
 }
