48c9ed28c0d8384ff5eb9f02bf61da2e4e7e2dfc
tkawamura <tkawamura.stayhome@gmail.com>
2022-08-21 19:00:19 +0900

    meta情報を整理

--- iberico/components/meta.tsx
+++ iberico/components/meta.tsx
@@ -34,7 +34,7 @@ const Meta = () => {
       <link rel="alternate" type="application/rss+xml" href="/feed.xml" />
       <meta
         name="description"
-        content={`A statically generated blog example using Next.js.`}
+        content={`Tech Blog Authored By T.Kawa`}
       />
       <meta property="og:image" content={HOME_OG_IMAGE_URL} />
     </Head>
